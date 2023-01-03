d651447444f04218104957422a13af58bd68830e
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2023-01-04 00:11:12 +0800

    Add files via upload

---
+++ day 79.py
@@ -0,0 +1,8 @@
+while True:
+    a = int(input("Masukkann Angka : "))
+
+    if a %2==0:
+        a = int(input("Masukkan Angka : "))
+    elif a %1==0:
+        print("You And I, End")
+        break
